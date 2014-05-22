d82a7c9637a908ff8cd35b27fb4216a316f46ba9
songhaikang <songhaikang@163.com>
2014-05-22 09:53:04 +0800

    调整项目框架添加BaseFrame-web
    调整项目框架添加BaseFrame-web

---
+++ web/base-frame/BaseFrame/BaseFrame-web/src/main/java/com/shk/baseframe/web/uc/domain/UserInfo.java
@@ -0,0 +1,31 @@
+package com.shk.baseframe.web.uc.domain;
+
+import com.shk.baseframe.api.uc.domain.UcUserInfo;
+
+/**
+ * @author: Ivan Vigoss
+ * Date: 14-2-20
+ * Time: AM11:11
+ */
+public class UserInfo extends UcUserInfo {
+
+    private String Id;
+
+    private String oper;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getOper() {
+        return oper;
+    }
+
+    public void setOper(String oper) {
+        this.oper = oper;
+    }
+}
